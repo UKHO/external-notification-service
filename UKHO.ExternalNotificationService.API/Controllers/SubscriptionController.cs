7c98b88e152c8e5a761d3815ed3e61a5dd36ba9e
usha13529 <usha13529@Mastek.com>
2022-01-19 11:47:34 +0530

    AB#-24140 - Added new solution and targeted to .net6.0 framework

---
+++ UKHO.ExternalNotificationService.API/Controllers/SubscriptionController.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace UKHO.ExternalNotificationService.API.Controllers
+{
+    [ApiController]
+    public class SubscriptionController : ControllerBase
+    {
+        public SubscriptionController()
+        {
+
+        }
+
+        [HttpPost]
+        [Route("/api/subscription")]
+        public async Task<IActionResult> Post(JObject jobj)
+        {
+            return Ok();
+        }
+    }
+}
