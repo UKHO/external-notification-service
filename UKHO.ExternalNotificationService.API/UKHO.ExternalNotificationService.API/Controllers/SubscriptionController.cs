194e8753e44a9bbf9573d3d8f239e44b83f84280
Ashish Shelar <ashish15251@mastek.com>
2022-01-20 19:41:20 +0530

    AB# 24140 Initial solution setup changes for ENS.

---
+++ UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.API/Controllers/SubscriptionController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace UKHO.ExternalNotificationService.API.Controllers
+{
+    [ApiController]
+    public class SubscriptionController : ControllerBase
+    {
+        public SubscriptionController()
+        {
+
+        }
+
+        [HttpPost]
+        [Route("/api/subscription")]
+        public async Task<IActionResult> Post([FromBody] JObject jobj)
+        {
+            return Ok();
+        }
+    }
+}
