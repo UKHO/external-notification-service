73ff34d3c5f037ab1ae1a5f28ef6f15025055819
Tejasi Chavan <tejasi15072@mastek.com>
2022-01-24 19:28:33 +0530

    feature/33893- changes in azure.tf and main.tf file

--- Deployment/src/main.tf
+++ Deployment/src/main.tf
@@ -1,17 +1,3 @@
-////////////
-data "azurerm_subnet" "main_subnet" {
-  name                 = var.spoke_subnet_name
-  virtual_network_name = var.spoke_vnet_name
-  resource_group_name  = var.spoke_rg
-}
-
-data "azurerm_subnet" "agent_subnet" {
-  provider             = azurerm.build_agent
-  name                 = var.agent_subnet_name
-  virtual_network_name = var.agent_vnet_name
-  resource_group_name  = var.agent_rg
-}
-////////////
 module "user_identity" {
   source              = "./Modules/UserIdentity"
   resource_group_name = azurerm_resource_group.rg.name
@@ -34,9 +20,6 @@ module "eventhub" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   logstashStorageName = lower("${local.service_name}logstash${local.env_name}")
-  m_spoke_subnet      = data.azurerm_subnet.main_subnet.id   ---?
-  agent_subnet        = data.azurerm_subnet.agent_subnet.id  ---?
-  allowed_ips         = var.allowed_ips                      ---?
   tags                = local.tags
 }
 
@@ -45,7 +28,6 @@ module "webapp_service" {
   name                      = local.web_app_name
   resource_group_name       = azurerm_resource_group.rg.name
   location                  = azurerm_resource_group.rg.location
-  subnet_id                 = data.azurerm_subnet.main_subnet.id ---?
   user_assigned_identity    = module.user_identity.ens_service_identity_id
   app_service_sku           = var.app_service_sku[local.env_name]
   app_settings = {
@@ -58,7 +40,6 @@ module "webapp_service" {
     "WEBSITE_ENABLE_SYNC_UPDATE_SITE"                      = "true"
   }
   tags                      = local.tags
-  allowed_ips               = var.allowed_ips
 }
 
 module "key_vault" {
@@ -68,9 +49,6 @@ module "key_vault" {
   env_name            = local.env_name
   tenant_id           = module.user_identity.ess_service_identity_tenant_id
   location            = azurerm_resource_group.rg.location
-  allowed_ips         = var.allowed_ips--?
-  subnet_id           = data.azurerm_subnet.main_subnet.id--?
-  agent_subnet        = data.azurerm_subnet.agent_subnet.id--?
   read_access_objects = {
     "ess_service_identity" = module.user_identity.ess_service_identity_principal_id
   }
