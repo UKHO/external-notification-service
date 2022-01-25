a35e1ce09de5cc32cda9ed936b46a4f0c10039a5
Ashish15251 <94688975+Ashish15251@users.noreply.github.com>
2022-01-25 14:45:43 +0530

    24140 new ens initial project solution setup changes (#2)
    * AB# 24140 Initial solution setup changes for ENS.
    
    * AB#-24140 - Added appsettings.local.overrides.json for local settings and removed appsettings.Development.json
    
    * AB# 24140 - new changes implemented.
    
    * AB#-24140 - Added Deployment folder and terraform_run.ps1
    
    * AB# 24140 - Removed unwanted namespaces.
    
    * AB#-24140 - Changed target framework to net6.0 for FunctionalTests project and added coverlet.msbuild package reference to unit tests project.
    
    * Update UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.API.UnitTests/Controllers/SubscriptionControllerTests.cs
    
    Co-authored-by: barrie-cooper <barrie.cooper@ukho.gov.uk>
    
    * Update UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.API.UnitTests/Controllers/SubscriptionControllerTests.cs
    
    Co-authored-by: barrie-cooper <barrie.cooper@ukho.gov.uk>
    
    * Update UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.API.UnitTests/Controllers/SubscriptionControllerTests.cs
    
    Co-authored-by: barrie-cooper <barrie.cooper@ukho.gov.uk>
    
    * AB# 24140 - Editor config moved to root branch.
    
    Co-authored-by: usha13529 <usha13529@Mastek.com>
    Co-authored-by: barrie-cooper <barrie.cooper@ukho.gov.uk>

---
+++ UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.API/Controllers/SubscriptionController.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace UKHO.ExternalNotificationService.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SubscriptionController : ControllerBase
+    {
+        public SubscriptionController()
+        {
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] JObject jobj)
+        {
+            return Ok();
+        }
+    }
+}
