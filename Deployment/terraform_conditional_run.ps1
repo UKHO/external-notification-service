fefc424d72333cf6395a6d5d2cde257453572b05
Tejasi Chavan <tejasi15072@mastek.com>
2022-01-27 22:10:10 +0530

    feature/33893- changes for testing

--- Deployment/terraform_conditional_run.ps1
+++ Deployment/terraform_conditional_run.ps1
@@ -49,7 +49,6 @@ if ( !$? ) { echo "Something went wrong during terraform apply" ; throw "Error" 
 Write-output "Terraform output as json"
 $terraformOutput = terraform output -json | ConvertFrom-Json
 Write-output "$terraformOutput"
-"
 
 write-output "Set JSON output into pipeline variables"
 Write-Host "##vso[task.setvariable variable=WEB_APP_NAME]$($terraformOutput.web_app_name.value)"
