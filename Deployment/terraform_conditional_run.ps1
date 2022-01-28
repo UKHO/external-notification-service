3f7faf4eeaa4f883c06b4b06ff112905cbaca13d
Tejasi Chavan <tejasi15072@mastek.com>
2022-01-28 10:37:57 +0530

    feature/33893- revert the testing changes

--- Deployment/terraform_conditional_run.ps1
+++ Deployment/terraform_conditional_run.ps1
@@ -48,14 +48,9 @@ if ( !$? ) { echo "Something went wrong during terraform apply" ; throw "Error" 
 
 Write-output "Terraform output as json"
 $terraformOutput = terraform output -json | ConvertFrom-Json
-Write-output "$terraformOutput"
 
 write-output "Set JSON output into pipeline variables"
 Write-Host "##vso[task.setvariable variable=WEB_APP_NAME]$($terraformOutput.web_app_name.value)"
 Write-Host "##vso[task.setvariable variable=EnsApiUrl]$env:SERVICE_DNS_URL"
-write-output "test"
+
 $terraformOutput | ConvertTo-Json -Depth 5 > $terraformJsonOutputFile
-cat "$terraformJsonOutputFile"
-write-output "print"
-$terraformOutput | ConvertTo-Json -Depth 2 > $terraformJsonOutputFile
-cat "$terraformJsonOutputFile"
