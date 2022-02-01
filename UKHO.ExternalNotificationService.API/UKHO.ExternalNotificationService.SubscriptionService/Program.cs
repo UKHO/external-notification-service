1170530b1c0be3d9902699a5bfb212962181f83c
Siddartha Mote <siddart14393@mastek.com>
2022-02-01 13:39:42 +0530

    24147 Initial skeleton for web jon AB#24147 AB#34688

---
+++ UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.SubscriptionService/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UKHO.ExternalNotificationService.SubscriptionService
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("testing console running or not");
+        }
+    }
+}
