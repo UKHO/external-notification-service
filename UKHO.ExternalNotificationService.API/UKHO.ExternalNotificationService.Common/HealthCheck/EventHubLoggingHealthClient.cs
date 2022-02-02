e9ba9e4efa7c7fc9544a781645c98c5c895549be
Barrie Cooper <barrie.cooper@ukho.gov.uk>
2022-02-02 18:23:03 +0000

    Fix so that Logstash will not error when receiving event hub health check

--- UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.Common/HealthCheck/EventHubLoggingHealthClient.cs
+++ UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.Common/HealthCheck/EventHubLoggingHealthClient.cs
@@ -1,15 +1,18 @@
-﻿
-using Azure.Messaging.EventHubs;
-using Azure.Messaging.EventHubs.Producer;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
-using Microsoft.Extensions.Options;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using UKHO.ExternalNotificationService.Common.Configuration;
 using UKHO.ExternalNotificationService.Common.Logging;
+using UKHO.Logging.EventHubLogProvider;
 
 namespace UKHO.ExternalNotificationService.Common.HealthCheck
 {
@@ -26,11 +29,32 @@ namespace UKHO.ExternalNotificationService.Common.HealthCheck
         public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
         {
             var eventHubProducerClient = new EventHubProducerClient(_eventHubLoggingConfiguration.Value.ConnectionString, _eventHubLoggingConfiguration.Value.EntityPath);
-            using EventDataBatch eventBatch = await eventHubProducerClient.CreateBatchAsync(cancellationToken);
-            eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes(EventIds.EventHubLoggingEventDataForHealthCheck.ToEventId() + " of Event Hub")));
+
             try
-            {         
-                await eventHubProducerClient.SendAsync(eventBatch,cancellationToken);
+            {
+                var logEntry = new LogEntry
+                {
+                    Timestamp = DateTime.UtcNow,
+                    Level = LogLevel.Trace.ToString(),
+                    MessageTemplate = "Event Hub Logging Event Data For Health Check",
+                    LogProperties = new Dictionary<string, object>
+                    {
+                        { "_Environment", _eventHubLoggingConfiguration.Value.Environment },
+                        { "_System",      _eventHubLoggingConfiguration.Value.System },
+                        { "_Service",     _eventHubLoggingConfiguration.Value.Service },
+                        { "_NodeName",    _eventHubLoggingConfiguration.Value.NodeName }
+                    },
+                    EventId = EventIds.EventHubLoggingEventDataForHealthCheck.ToEventId()
+                };
+
+                string jsonLogEntry = JsonConvert.SerializeObject(logEntry);
+
+                using EventDataBatch eventBatch = await eventHubProducerClient.CreateBatchAsync(cancellationToken);
+
+                eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes(jsonLogEntry)));
+
+                await eventHubProducerClient.SendAsync(eventBatch, cancellationToken);
+
                 return HealthCheckResult.Healthy("Event hub is healthy");
             }
             catch (Exception ex)
