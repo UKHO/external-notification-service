1f58c7244d8b76a1e633cbb077da9d9c3b2b4ef8
Shirin14926 <Shirin.talawdekar@mastek.com>
2022-02-03 17:23:01 +0530

    24145 fix review comment AB#24145 AB#33921

--- UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.API/Program.cs
+++ UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.API/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Hosting;
 namespace UKHO.ExternalNotificationService.API
 {
     [ExcludeFromCodeCoverage]
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
