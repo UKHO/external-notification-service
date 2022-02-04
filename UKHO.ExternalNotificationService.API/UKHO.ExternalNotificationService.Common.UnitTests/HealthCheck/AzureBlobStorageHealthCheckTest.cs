01c454866ef419b87477c1b980104deab50e4857
Vipin Mahajan <vipin14904@mastek.com>
2022-02-04 15:49:30 +0530

    34643- Coding  AB#34643 AB#34844

---
+++ UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.Common.UnitTests/HealthCheck/AzureBlobStorageHealthCheckTest.cs
@@ -0,0 +1,56 @@
+﻿
+using System.Threading.Tasks;
+using FakeItEasy;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using NUnit.Framework;
+using UKHO.ExternalNotificationService.Common.Configuration;
+using UKHO.ExternalNotificationService.Common.HealthCheck;
+using UKHO.ExternalNotificationService.Common.Helpers;
+using UKHO.ExternalNotificationService.Common.Storage;
+
+namespace UKHO.ExternalNotificationService.Common.UnitTests.HealthCheck
+{
+    [TestFixture]
+    public class AzureBlobStorageHealthCheckTest
+    {
+        private IAzureBlobStorageHelper _fakeAzureBlobStorageHelper;
+        private IStorageService _fakeStorageService;
+        private IOptions<EnsFulfilmentStorageConfiguration> _fakeEnsFulfilmentStorageConfiguration;
+        private ILogger<AzureBlobStorageHealthCheck> _fakeLogger;
+        private AzureBlobStorageHealthCheck _azureBlobStorageHealthCheck;
+        
+
+        [SetUp]
+        public void Setup()
+        {
+            _fakeAzureBlobStorageHelper = A.Fake<IAzureBlobStorageHelper>();
+            _fakeStorageService= A.Fake<IStorageService>();
+            _fakeEnsFulfilmentStorageConfiguration = A.Fake<IOptions<EnsFulfilmentStorageConfiguration>>();
+            _fakeLogger = A.Fake<ILogger<AzureBlobStorageHealthCheck>>();
+
+            _azureBlobStorageHealthCheck = new AzureBlobStorageHealthCheck(_fakeAzureBlobStorageHelper, _fakeStorageService,_fakeEnsFulfilmentStorageConfiguration,_fakeLogger);
+        }
+
+        [Test]
+        public async Task WhenBlobStorageIsHealthy_ThenReturnsHealthy()
+        {
+            A.CallTo(() => _fakeAzureBlobStorageHelper.CheckBlobContainerHealth(A<string>.Ignored, A<string>.Ignored)).Returns(new HealthCheckResult(HealthStatus.Healthy));
+
+            var response = await _azureBlobStorageHealthCheck.CheckHealthAsync(new HealthCheckContext());
+
+            Assert.AreEqual(HealthStatus.Healthy, response.Status);
+        }
+
+        [Test]
+        public async Task WhenBlobStorageIsUnhealthy_ThenReturnsUnhealthy()
+        {
+            A.CallTo(() => _fakeAzureBlobStorageHelper.CheckBlobContainerHealth(A<string>.Ignored, A<string>.Ignored)).Returns(new HealthCheckResult(HealthStatus.Unhealthy));
+
+            var response = await _azureBlobStorageHealthCheck.CheckHealthAsync(new HealthCheckContext());
+
+            Assert.AreEqual(HealthStatus.Unhealthy, response.Status);
+        }
+    }
+}
---
+++ UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.Common.UnitTests/HealthCheck/AzureMessageQueueHealthCheckTest.cs
@@ -0,0 +1,55 @@
+﻿
+using System.Threading.Tasks;
+using FakeItEasy;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using NUnit.Framework;
+using UKHO.ExternalNotificationService.Common.Configuration;
+using UKHO.ExternalNotificationService.Common.HealthCheck;
+using UKHO.ExternalNotificationService.Common.Helpers;
+using UKHO.ExternalNotificationService.Common.Storage;
+
+namespace UKHO.ExternalNotificationService.Common.UnitTests.HealthCheck
+{
+    public class AzureMessageQueueHealthCheckTest
+    {
+        private IOptions<EnsFulfilmentStorageConfiguration> _fakeEnsFulfilmentStorageConfiguration;
+        private ILogger<AzureMessageQueueHelper> _fakeLogger;
+        private IStorageService _fakeStorageService;
+        private IAzureMessageQueueHelper _fakeAzureMessageQueueHelper;
+        private AzureMessageQueueHealthCheck _azureMessageQueueHealthCheck;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _fakeEnsFulfilmentStorageConfiguration = A.Fake<IOptions<EnsFulfilmentStorageConfiguration>>();
+            _fakeLogger = A.Fake<ILogger<AzureMessageQueueHelper>>();
+            _fakeStorageService = A.Fake<IStorageService>();
+            _fakeAzureMessageQueueHelper = A.Fake<IAzureMessageQueueHelper>();
+
+            _azureMessageQueueHealthCheck = new AzureMessageQueueHealthCheck(_fakeEnsFulfilmentStorageConfiguration,_fakeLogger,_fakeStorageService,_fakeAzureMessageQueueHelper);
+        }
+
+        [Test]
+        public async Task WhenMessageQueueIsHealthy_ThenReturnsHealthy()
+        {
+            A.CallTo(() => _fakeAzureMessageQueueHelper.CheckMessageQueueHealth(A<string>.Ignored, A<string>.Ignored)).Returns(new HealthCheckResult(HealthStatus.Healthy));
+
+            var response = await _azureMessageQueueHealthCheck.CheckHealthAsync(new HealthCheckContext());
+
+            Assert.AreEqual(HealthStatus.Healthy, response.Status);
+        }
+
+        [Test]
+        public async Task WhenMessageQueueIsUnhealthy_ThenReturnsUnhealthy()
+        {
+            A.CallTo(() => _fakeAzureMessageQueueHelper.CheckMessageQueueHealth(A<string>.Ignored, A<string>.Ignored)).Returns(new HealthCheckResult(HealthStatus.Unhealthy));
+
+            var response = await _azureMessageQueueHealthCheck.CheckHealthAsync(new HealthCheckContext());
+
+            Assert.AreEqual(HealthStatus.Unhealthy, response.Status);
+        }
+    }
+}
