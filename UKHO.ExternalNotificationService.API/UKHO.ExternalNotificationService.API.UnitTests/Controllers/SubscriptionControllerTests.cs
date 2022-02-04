17a00c250c401a113715c540acd72bde9230a620
Shirin14926 <85286507+Shirin14926@users.noreply.github.com>
2022-02-04 16:01:35 +0530

    Feature/24145-Build_Initial_Notification_Subscription_Webhook_Endpoint (#12)
    * 24145-coding and unit test case  AB#24145 AB#33921
    
    * 24145 code coverage and code optimization AB#24145 AB#33922
    
    * 24145 -modify unit test cases and log in endpoint AB#24145 AB#33922
    
    * 24145 fix review comment AB#24145 AB#33921
    
    * 24145- fix review comment AB#24145 AB#33921
    
    * 24145 rename testcase AB#24145 AB#33921

--- UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.API.UnitTests/Controllers/SubscriptionControllerTests.cs
+++ UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.API.UnitTests/Controllers/SubscriptionControllerTests.cs
@@ -3,9 +3,9 @@ using FakeItEasy;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using UKHO.ExternalNotificationService.API.Controllers;
+using UKHO.ExternalNotificationService.API.Models;
 
 namespace UKHO.ExternalNotificationService.API.UnitTests.Controllers
 {
@@ -22,15 +22,34 @@ namespace UKHO.ExternalNotificationService.API.UnitTests.Controllers
             _fakeHttpContextAccessor = A.Fake<IHttpContextAccessor>();
             _fakeLogger = A.Fake<ILogger<SubscriptionController>>();
             A.CallTo(() => _fakeHttpContextAccessor.HttpContext).Returns(new DefaultHttpContext());
+
             _controller = new SubscriptionController(_fakeHttpContextAccessor, _fakeLogger);
         }
 
         [Test]
-        public async Task TestSubscription()
+        public async Task WhenPostValidPayload_ThenRecieveSuccessfulResponse()
         {
-            dynamic jsonObject = new JObject();
-            var result = await _controller.Post(jsonObject);
-            Assert.IsInstanceOf<OkResult>(result);
+            var result = (StatusCodeResult)await _controller.Post(GetD365Payload());
+            Assert.AreEqual(StatusCodes.Status202Accepted, result.StatusCode);
+        }
+
+        private static D365Payload GetD365Payload()
+        {
+            return new D365Payload
+            {
+                CorrelationId = "7b4cdb10-ddfd-4ed6-b2be-d1543d8b7272",
+                OperationCreatedOn = "Date(1000097000 + 0000)",
+                InputParameters = new InputParameter[] { new InputParameter { Value = new InputParameterValue
+                {
+                    Attributes = new D365Attribute[] { new D365Attribute { Key = "subscribedacc", Value = "test" }, new D365Attribute{ Key = "test_name", Value = "Clay" }},
+                    FormattedValues = new FormattedValue[] { new FormattedValue { Key ="state", Value = "Active"}, new FormattedValue{ Key = "acc", Value = "A"}}
+                }}},
+                PostEntityImages = new EntityImage[] { new EntityImage { Key = "AsynchronousTestName" , ImageValue = new EntityImageValue
+                {
+                    Attributes = new D365Attribute[] { new D365Attribute { Key = "subscribedacc", Value = "test" }, new D365Attribute{ Key = "test_name", Value = "Clay" }},
+                    FormattedValues = new FormattedValue[] { new FormattedValue { Key ="state", Value = "Active"}, new FormattedValue{ Key = "acc", Value = "A"}}
+                }}}
+            };
         }
     }
 }
--- UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.API/Program.cs
+++ UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.API/Program.cs
@@ -1,9 +1,11 @@
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
 namespace UKHO.ExternalNotificationService.API
 {
-    public class Program
+    [ExcludeFromCodeCoverage]
+    public static class Program
     {
         public static void Main(string[] args)
         {
