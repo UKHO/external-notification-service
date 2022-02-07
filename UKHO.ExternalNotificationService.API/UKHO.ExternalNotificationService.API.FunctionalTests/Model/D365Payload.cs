2e93d256621f36d1b69995eb6f1d24200c7ddf44
Ganpat Gawde <Ganpat14802@Mastek.Com>
2022-02-07 14:53:08 +0530

    34054 -minor function test case changes AB#34054  AB#34811

--- UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.API.FunctionalTests/Model/D365Payload.cs
+++ UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.API.FunctionalTests/Model/D365Payload.cs
@@ -1,43 +1,54 @@
-﻿
+﻿using System.Text.Json.Serialization;
+
 namespace UKHO.ExternalNotificationService.API.FunctionalTests.Model
 {
     public class D365Payload
     {
-        public string CorrelationId;
-        public string OperationCreatedOn;
-        public InputParameter[] InputParameters;
-        public EntityImage[] PostEntityImages;
+        public string CorrelationId { get; set; }
+        public string OperationCreatedOn { get; set; }
+        public EntityImage[] PostEntityImages { get; set; }
+        public InputParameter[] InputParameters { get; set; }
     }
 
-    public class EntityImage
-    {
-        public string key;
-        public EntityImageValue value;
-    }
-
-    public class EntityImageValue
-    {
-        public D365Attribute[] Attributes;
-        public FormattedValue[] FormattedValues;
-    }
     public class InputParameter
     {
-        public InputParameterValue value;
+        [JsonPropertyName("value")]
+        public InputParameterValue Value { get; set; }
     }
+
     public class InputParameterValue
     {
-        public D365Attribute[] Attributes;
-        public FormattedValue[] FormattedValues;
+        public D365Attribute[] Attributes { get; set; }
+        public FormattedValue[] FormattedValues { get; set; }
     }
 
     public class FormattedValue
     {
-        public string key;
-        public object value;
+        [JsonPropertyName("key")]
+        public string Key { get; set; }
+        [JsonPropertyName("value")]
+        public object Value { get; set; }
     }
+
     public class D365Attribute
     {
-        public string key;
-        public object value;
+        [JsonPropertyName("key")]
+        public string Key { get; set; }
+
+        [JsonPropertyName("value")]
+        public object Value { get; set; }
+    }
+
+    public class EntityImage
+    {
+        [JsonPropertyName("key")]
+        public string Key { get; set; }
+        public EntityImageValue ImageValue { get; set; }
+    }
+
+    public class EntityImageValue
+    {
+        public D365Attribute[] Attributes { get; set; }
+        public FormattedValue[] FormattedValues { get; set; }
     }
 }
