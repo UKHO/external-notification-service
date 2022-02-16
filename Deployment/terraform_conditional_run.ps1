bc5b246c0db42211d5d1dcb8b882726b2f005272
Amogh Lanke <amogh.lanke@mastek.conm>
2022-02-16 19:43:55 +0530

    Feature/24147 changes for pipeline variables -added missing double quotes

--- Deployment/terraform_conditional_run.ps1
+++ Deployment/terraform_conditional_run.ps1
@@ -57,6 +57,6 @@ Write-Host "##vso[task.setvariable variable=EventHubLoggingConfiguration.Environ
 Write-Host "##vso[task.setvariable variable=EnsStorageConnectionString;issecret=true]$($terraformOutput.ens_storage_connection_string.value)"
 Write-Host "##vso[task.setvariable variable=EnsStorageQueueName]$($terraformOutput.event_storage_queue.value)"
 Write-Host "##vso[task.setvariable variable=EventGridDomainConfiguration.ResourceGroup]$($terraformOutput.resource_group_name.value)"
-Write-Host "##vso[task.setvariable variable=EventGridDomainConfiguration.EventGridDomainName]$($terraformOutput.event_grid_doamin_name.value)
+Write-Host "##vso[task.setvariable variable=EventGridDomainConfiguration.EventGridDomainName]$($terraformOutput.event_grid_doamin_name.value)"
 
 $terraformOutput | ConvertTo-Json -Depth 5 > $terraformJsonOutputFile
