1fba8b21dec9c289a54a9df887bf7381a657276d
Vipin Mahajan <vipin14904@mastek.com>
2022-02-17 20:23:02 +0530

    24156-initial project setup  AB#42368

---
+++ UKHO.D365CallbackDistributorStub.API/UKHO.D365CallbackDistributorStub.API/Controllers/DistributorController.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace UKHO.D365CallbackDistributorStub.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DistributorController : ControllerBase
+    {
+        public async virtual Task<IActionResult> Post()
+        {
+            return Ok();
+        }
+    }
+}
