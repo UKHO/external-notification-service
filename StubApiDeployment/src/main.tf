54705eb79bff7edcf63ff291cec076cf04448282
Tejasi Chavan <tejasi15072@mastek.com>
2022-02-22 22:24:01 +0530

    terraform changes for stub webapp #AB24156

--- StubApiDeployment/src/main.tf
+++ StubApiDeployment/src/main.tf
@@ -2,7 +2,6 @@ module "webapp_service" {
   source              = "./Modules/Webapp"
   name                = local.web_app_name
   resource_group_name = var.ens_api_rg
-  ens_api_asp         = var.ens_api_asp
   location            = azurerm_resource_group.rg.location
   app_service_sku     = var.app_service_sku[local.env_name]
   app_settings = {
