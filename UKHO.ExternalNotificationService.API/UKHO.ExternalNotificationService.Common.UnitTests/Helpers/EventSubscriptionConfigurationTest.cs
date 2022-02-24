285f39276a27768e569d665a6c6b41920aea6da4
Siddartha Mote <siddart14393@mastek.com>
2022-02-24 16:58:17 +0530

    24148 made changes with casting validating details of EventSubscription AB#24148 AB#42840

--- UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.Common.UnitTests/Helpers/EventSubscriptionConfigurationTest.cs
+++ UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.Common.UnitTests/Helpers/EventSubscriptionConfigurationTest.cs
@@ -15,7 +15,12 @@ namespace UKHO.ExternalNotificationService.Common.UnitTests.Helpers
         private IOptions<EventGridDomainConfiguration> _fakeEventGridDomainConfig;
         private IOptions<SubscriptionStorageConfiguration> _fakeSubscriptionStorageConfiguration;
         private EventSubscriptionConfiguration _eventSubscriptionConfiguration;
-
+        private const string SubscriptionId = "EventDomainSubscriptionId";
+        private const string ResourceGroup = "rg";
+        private const string StorageAccountName = "StorageAccountName";
+        private const string StorageContainerName = "StorageContainerName";
+        private const string WebhookUrl = "https://abc.com/";
+        private const string ExpectedDeadLetterDestinationResourceId = $"/subscriptions/{SubscriptionId}/resourceGroups/{ResourceGroup}/providers/Microsoft.Storage/storageAccounts/{StorageAccountName}";
         [SetUp]
         public void Setup()
         {
@@ -23,36 +28,37 @@ namespace UKHO.ExternalNotificationService.Common.UnitTests.Helpers
             _fakeSubscriptionStorageConfiguration = A.Fake<IOptions<SubscriptionStorageConfiguration>>();
             _fakeEventGridDomainConfig.Value.MaxDeliveryAttempts = 10;
             _fakeEventGridDomainConfig.Value.EventTimeToLiveInMinutes = 10;
-            _fakeEventGridDomainConfig.Value.SubscriptionId = "EventDomainSubscriptionId";
-            _fakeEventGridDomainConfig.Value.ResourceGroup = "rg";
-            _fakeSubscriptionStorageConfiguration.Value.StorageAccountName = "StorageAcountName";
-            _fakeSubscriptionStorageConfiguration.Value.StorageContainerName = "StorageContainerName";
-
+            _fakeEventGridDomainConfig.Value.SubscriptionId = SubscriptionId;
+            _fakeEventGridDomainConfig.Value.ResourceGroup = ResourceGroup;
+            _fakeSubscriptionStorageConfiguration.Value.StorageAccountName = StorageAccountName;
+            _fakeSubscriptionStorageConfiguration.Value.StorageContainerName = StorageContainerName;
             _eventSubscriptionConfiguration = new EventSubscriptionConfiguration(_fakeEventGridDomainConfig, _fakeSubscriptionStorageConfiguration);
         }
-
         [Test]
         public void WhenCallSetEventSubscription_ThenReturnEventSubscription()
         {
-            SubscriptionRequestMessage subscriptionRequestMessage = new() { 
+            SubscriptionRequestMessage subscriptionRequestMessage = new()
+            {
                 CorrelationId = Guid.NewGuid().ToString(),
                 D365CorrelationId = Guid.NewGuid().ToString(),
                 IsActive = true,
                 NotificationType = "ADDS Data Pipeline",
                 NotificationTypeTopicName = "avcs-contentPublished",
                 SubscriptionId = Guid.NewGuid().ToString(),
-                WebhookUrl = "https://abc.com/"
+                WebhookUrl = WebhookUrl
             };
-
             EventSubscription result = _eventSubscriptionConfiguration.SetEventSubscription(subscriptionRequestMessage);
             Assert.IsNotNull(result);
             Assert.IsInstanceOf<EventSubscription>(result);
             Assert.IsInstanceOf<EventSubscriptionDestination>(result.Destination);
+            Assert.AreEqual(WebhookUrl, ((WebHookEventSubscriptionDestination)result.Destination).EndpointUrl);
+            Assert.AreEqual(EventDeliverySchema.CloudEventSchemaV10, result.EventDeliverySchema);
             Assert.IsInstanceOf<RetryPolicy>(result.RetryPolicy);
             Assert.AreEqual(_fakeEventGridDomainConfig.Value.MaxDeliveryAttempts, result.RetryPolicy.MaxDeliveryAttempts);
             Assert.AreEqual(_fakeEventGridDomainConfig.Value.EventTimeToLiveInMinutes, result.RetryPolicy.EventTimeToLiveInMinutes);
             Assert.IsInstanceOf<DeadLetterDestination>(result.DeadLetterDestination);
-            Assert.AreEqual(EventDeliverySchema.CloudEventSchemaV10, result.EventDeliverySchema);
+            Assert.AreEqual(ExpectedDeadLetterDestinationResourceId, ((StorageBlobDeadLetterDestination)result.DeadLetterDestination).ResourceId);
+            Assert.AreEqual(StorageContainerName, ((StorageBlobDeadLetterDestination)result.DeadLetterDestination).BlobContainerName);
         }
     }
 }
