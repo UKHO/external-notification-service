d6e5a29d7fdd16741e5ced6e262d826ae2d9e0f7
Amogh Lanke <amogh.lanke@mastek.conm>
2022-02-28 18:54:22 +0530

    Removed App Settings List changes from PS script. AB#24156 AB#42384

--- Deployment/set_stub_url.ps1
+++ Deployment/set_stub_url.ps1
@@ -3,10 +3,7 @@ param (
     [Parameter(Mandatory = $true)] [string] $resourceGroup,
     [Parameter(Mandatory = $true)] [string] $webappName
 )
-Write-Output "List ens webapp appsettings..."
-az webapp config appsettings list -g $resourceGroup -n $webappName
+
 Write-Output "Set dev D365 url in appsetting..."
 az webapp config appsettings set -g $resourceGroup -n $webappName --settings D365CallbackConfiguration:D365ApiUri=$stubUri
 az webapp restart --name $webappName --resource-group $resourceGroup
-Write-Output "List ens webapp appsettings after pointing to Dev environment..."
-az webapp config appsettings list -g $resourceGroup -n $webappName
