7d1927c5d4b207e168f207bf395c25b8edf5b83a
Amogh Lanke <amogh.lanke@mastek.conm>
2022-03-02 14:11:10 +0530

    AB#24166 Handled Event grid domain endpoint as pipeline variable

--- Deployment/terraform_conditional_run.ps1
+++ Deployment/terraform_conditional_run.ps1
@@ -60,5 +60,6 @@ Write-Host "##vso[task.setvariable variable=SubscriptionStorageConfiguration.Sto
 Write-Host "##vso[task.setvariable variable=stubWebAppName]$($terraformOutput.stub_webappname.value)"
 Write-Host "##vso[task.setvariable variable=D365CallbackConfiguration.D365ApiUri]$($terraformOutput.ens_stub_web_app_url.value)"
 Write-Host "##vso[task.setvariable variable=ResourceGroup]$($terraformOutput.webapp_rg.value)"
+Write-Host "##vso[task.setvariable variable=EventGridDomainConfiguration.EventGridDomainEndpoint;issecret=true]$($terraformOutput.event_grid_domain_endpoint_url.value)"
 
 $terraformOutput | ConvertTo-Json -Depth 5 > $terraformJsonOutputFile
