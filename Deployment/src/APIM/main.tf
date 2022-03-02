0043808e1f0d7942d5e117f75014a085a61c0f0a
vijayavelr <vijayavelr@IN-MH1DSW101716>
2022-03-02 13:14:12 +0530

    AB#35180-fixed foldername typo error

--- Deployment/src/APIM/main.tf
+++ Deployment/src/APIM/main.tf
@@ -1,5 +1,5 @@
 module "APIM" {
-  source					      = "./Modules/ExternalNotifcationService"
+  source					      = "./Modules/ExternalNotificationService"
   env						      = local.env_name  
   env_suffix                      = var.env_suffix_text[local.env_name]
   api_openapi_spec_path           = file("${path.module}/external-notification-service.openApi.yaml")
