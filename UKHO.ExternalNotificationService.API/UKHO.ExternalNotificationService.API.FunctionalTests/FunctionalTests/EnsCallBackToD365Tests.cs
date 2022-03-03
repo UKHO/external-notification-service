96313d419437d169bd1a0ef872119c698abfa6a2
jigar15426 <jigar15426@IN-MH1LPW102766>
2022-03-03 19:07:09 +0530

    34472-added multiple subcription id case AB#43173

--- UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.API.FunctionalTests/FunctionalTests/EnsCallBackToD365Tests.cs
+++ UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.API.FunctionalTests/FunctionalTests/EnsCallBackToD365Tests.cs
@@ -77,7 +77,29 @@ namespace UKHO.ExternalNotificationService.API.FunctionalTests.FunctionalTests
 
         }
 
+        [Test]
+        public async Task WhenICallTheEnsSubscriptionApiWithAMultipleSubscriptionId_ThenSucessStatusResponseIsRetruns()
+        {
+          
+           // Get the subscriptionId from D365 payload for first subcription id
+            string subscriptionId = D365Payload.PostEntityImages[0].Value.Attributes[0].Value.ToString();
 
-    
+            HttpResponseMessage apiResponse = await EnsApiClient.PostEnsApiSubscriptionAsync(D365Payload, EnsToken);
+            Assert.AreEqual(202, (int)apiResponse.StatusCode, $"Incorrect status code {apiResponse.StatusCode}  is  returned, instead of the expected 202.");
+
+            HttpResponseMessage newapiResponse = await EnsApiClient.PostEnsApiSubscriptionAsync(D365Payload, EnsToken);
+            Assert.AreEqual(202, (int)newapiResponse.StatusCode, $"Incorrect status code {newapiResponse.StatusCode}  is  returned, instead of the expected 202.");
+
+            HttpResponseMessage callBackResponse = await EnsApiClient.GetEnsCallBackAsync(TestConfig.D365ApiUri, subscriptionId);
+            Assert.AreEqual(200, (int)callBackResponse.StatusCode, $"Incorrect status code {callBackResponse.StatusCode}  is  returned, instead of the expected 200.");
+
+            EnsCallbackResponseDetailsModel callBackResponseBody = await callBackResponse.ReadAsTypeAsync<EnsCallbackResponseDetailsModel>();
+
+            foreach (EnsCallbackResponseModel entry in callBackResponseBody.CallBackRequests)
+            {
+                Assert.AreEqual(subscriptionId, entry.subscriptionId, $"Invalid subscriptionId {entry.subscriptionId} , Instead of expected subscriptionId {subscriptionId}.");
+                Assert.AreEqual(TestConfig.SucceededStatusCode, entry.callBackRequest.ukho_lastresponse, $"Invalid last response {TestConfig.SucceededStatusCode} , Instead of expected last response {entry.callBackRequest.ukho_lastresponse}.");
+            }
+        }
     }
 }
