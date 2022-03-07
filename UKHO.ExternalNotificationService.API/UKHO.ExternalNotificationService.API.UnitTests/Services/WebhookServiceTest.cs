d07713346fb7c43537d46bd28556b3ee00d5f931
Ganpat Gawde <Ganpat14802@Mastek.Com>
2022-03-07 16:26:12 +0530

    24166 added unit test cases for webhook service AB#24166 AB#43124

---
+++ UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.API.UnitTests/Services/WebhookServiceTest.cs
@@ -0,0 +1,43 @@
+﻿using FakeItEasy;
+using NUnit.Framework;
+using System.Collections.Generic;
+using UKHO.ExternalNotificationService.API.Services;
+
+namespace UKHO.ExternalNotificationService.API.UnitTests.Services
+{
+    [TestFixture]
+    public class WebhookServiceTest
+    {
+        private List<IEventProcessor> _fakeEventProcessors;
+        private WebhookService _webhookService;
+        private FssEventProcessor _fakeFssEventProcessor;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fakeFssEventProcessor = A.Fake<FssEventProcessor>();
+            _fakeEventProcessors = new List<IEventProcessor>
+            {
+                _fakeFssEventProcessor
+            };
+
+            _webhookService = new WebhookService(_fakeEventProcessors);
+        }
+
+        [Test]
+        public void WhenInvalidValidEventTypeInRequest_ThenReturnNullValue()
+        {
+            var result = _webhookService.GetProcessor("uk.gov.UKHO.NewFilesPublished.v1");
+
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void WhenValidPayloadInRequest_ThenReturnIEventProcessorService()
+        {
+            var result = _webhookService.GetProcessor("uk.gov.UKHO.FileShareService.NewFilesPublished.v1");
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
