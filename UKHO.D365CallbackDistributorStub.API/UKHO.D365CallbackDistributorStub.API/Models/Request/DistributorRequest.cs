cca0d8f719d54ca45d3254517c5534488c06bff6
vipin14904 <85299812+vipin14904@users.noreply.github.com>
2022-03-09 15:58:22 +0530

    42787 Stubs can be commanded to fail (#31)
    * AB#42787  Code changes AB#43216
    
    * AB#42787 return httpStatus AB#43216
    
    * AB#42787 Added logs AB#43216
    
    * AB#42787 Post to patch callback AB#43216
    
    * AB#42787 - Implemented code for Stub AB#42787 AB#43217
    
    * AB#42787 Code changes. AB#43217
    
    * AB#42787 Fixed Propert name
    
    * AB#42787 Remove body from json payload
    
    * AB#42787 - rename method & variable names.
    
    * AB# 42787 Change nomenclature for variable
    
    * AB#42787 Guid is coming 0000 in case of distributor
    
    * AB# Fixed review  comments
    
    * AB#42787 Rename variable name
    
    * AB#42787 timestamp as datetime
    
    * AB# Review comments
    
    * AB#42787 Client Code review comment resolved
    
    Co-authored-by: Ashish Shelar <ashish15251@mastek.com>

--- UKHO.D365CallbackDistributorStub.API/UKHO.D365CallbackDistributorStub.API/Models/Request/DistributorRequest.cs
+++ UKHO.D365CallbackDistributorStub.API/UKHO.D365CallbackDistributorStub.API/Models/Request/DistributorRequest.cs
@@ -1,9 +1,13 @@
-﻿namespace UKHO.D365CallbackDistributorStub.API.Models.Request
+﻿using System.Net;
+
+namespace UKHO.D365CallbackDistributorStub.API.Models.Request
 {
     public class DistributorRequest
     {
-        public CustomCloudEvent? CloudEvent {get;set;}
+        public CustomCloudEvent? CloudEvent { get; set; }
         public string? Subject { get; set; }
         public Guid Guid { get; set; }
+        public DateTime? TimeStamp { get; set; }
+        public HttpStatusCode? StatusCode { get; set; }
     }
 }
