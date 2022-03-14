820840fb617404edcdca9b61aebc2620eacad77d
jigar15426 <jigar15426@IN-MH1LPW102766>
2022-03-14 18:38:59 +0530

    34472 update the static url AB#44020

--- UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.API.FunctionalTests/Helper/FssEventDataBase.cs
+++ UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.API.FunctionalTests/Helper/FssEventDataBase.cs
@@ -7,31 +7,31 @@ namespace UKHO.ExternalNotificationService.API.FunctionalTests.Helper
 {
     public static class FssEventDataBase
     {
-        public static JObject GetFssEventBodyData()
+        public static JObject GetFssEventBodyData(TestConfiguration testConfigure)
         {
             var ensWebhookJson = JObject.Parse(@"{""Type"":""uk.gov.UKHO.FileShareService.NewFilesPublished.v1""}");
-            ensWebhookJson["Source"] = "https://files.admiralty.co.uk";
+            ensWebhookJson["Source"] = $"https://{testConfigure.FssEventHostName}";
             ensWebhookJson["Id"] = "49c67cca-9cca-4655-a38e-583693af55ea";
             ensWebhookJson["Subject"] = "83d08093-7a67-4b3a-b431-92ba42feaea0";
             ensWebhookJson["DataContentType"] = "application/json";
-            ensWebhookJson["Data"] = JObject.FromObject(GetFssEventData());
+            ensWebhookJson["Data"] = JObject.FromObject(GetFssEventData(testConfigure));
 
             return ensWebhookJson;
         }
-        public static FssEventData GetFssEventData()
+        public static FssEventData GetFssEventData(TestConfiguration testConfigure)
         {
             Link linkBatchDetails = new()
             {
-                Href = @"https://files.admiralty.co.uk/batch/83d08093-7a67-4b3a-b431-92ba42feaea0"
+                Href = $"https://{testConfigure.FssEventHostName}/batch/83d08093-7a67-4b3a-b431-92ba42feaea0"
             };
             Link linkBatchStatus = new()
             {
-                Href = @"https://files.admiralty.co.uk/batch/83d08093-7a67-4b3a-b431-92ba42feaea0/status"
+                Href = $"https://{testConfigure.FssEventHostName}/batch/83d08093-7a67-4b3a-b431-92ba42feaea0/status"
             };
 
             FileLinks fileLinks = new()
             {
-                Get = new Link() { Href = @"https://files.admiralty.co.uk/batch/83d08093-7a67-4b3a-b431-92ba42feaea0/files/AVCS_S631-1_Update_Wk45_21_Only.zip" },
+                Get = new Link() { Href = $"https://{testConfigure.FssEventHostName}/batch/83d08093-7a67-4b3a-b431-92ba42feaea0/files/AVCS_S631-1_Update_Wk45_21_Only.zip" },
             };
 
             BatchLinks links = new()
