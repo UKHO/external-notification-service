28591669ab4cbd06bcf89260efe27194f1bf2f93
Ganpat Gawde <Ganpat14802@Mastek.Com>
2022-03-15 20:08:55 +0530

    24167 added code changes AB#24167  AB#44126

---
+++ UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.API/Validation/ScsEventDataValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System.Net;
+using System.Threading.Tasks;
+using UKHO.ExternalNotificationService.Common.Models.EventModel;
+
+namespace UKHO.ExternalNotificationService.API.Validation
+{
+    public interface IScsEventDataValidator
+    {
+        Task<ValidationResult> Validate(ScsEventData scsEventData);
+    }
+
+    public class ScsEventDataValidator : AbstractValidator<ScsEventData>, IScsEventDataValidator
+    {
+        public ScsEventDataValidator()
+        {
+            RuleFor(v => v.ProductType).NotNull().NotEmpty()
+                .WithErrorCode(HttpStatusCode.OK.ToString())
+                .WithMessage("ProductType cannot be blank or null.");
+
+            RuleFor(v => v.ProductName).NotNull().NotEmpty()
+                .WithErrorCode(HttpStatusCode.OK.ToString())
+                .WithMessage("ProductName cannot be blank or null.");
+        }
+
+        Task<ValidationResult> IScsEventDataValidator.Validate(ScsEventData scsEventData)
+        {
+            return ValidateAsync(scsEventData);
+        }
+    }
+}
---
+++ UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.Common/Models/EventModel/ScsEventData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UKHO.ExternalNotificationService.Common.Models.EventModel
+{
+    public class ScsEventData
+    {
+        public string ProductType { get; set; }
+        public string ProductName { get; set; }
+        public int EditionNumber { get; set; }
+        public int UpdateNumber { get; set; }
+        public ProductBoundingBox BoundingBox { get; set; }
+        public ProductUpdateStatus Status { get; set; }
+        public bool IsPermitUpdateRequired { get; set; }
+        public long FileSize { get; set; }
+    }
+
+    public class ProductBoundingBox
+    {
+        public double NorthLimit { get; set; }
+        public double SouthLimit { get; set; }
+        public double EastLimit { get; set; }
+        public double WestLimit { get; set; }
+    }
+
+    public class ProductUpdateStatus
+    {
+        public DateTime StatusDate { get; set; }
+        public bool IsNewCell { get; set; }
+        public string StatusName { get; set; }
+    }
+}
