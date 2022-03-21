8489aa54c41a33cd9919e70c0ba623be6c59b1d8
usha13529 <usha13529@Mastek.com>
2022-03-21 12:59:42 +0530

    42126 - Changes related to Callback to D365 using managed identity

--- UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.SubscriptionService/Helpers/AuthTokenProvider.cs
+++ UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.SubscriptionService/Helpers/AuthTokenProvider.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Azure.Core;
+using Azure.Identity;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.Identity.Client;
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
-using UKHO.ExternalNotificationService.Common.Configuration;
 using UKHO.ExternalNotificationService.Common.Logging;
 using UKHO.ExternalNotificationService.Common.Models.Request;
 using UKHO.ExternalNotificationService.SubscriptionService.Configuration;
@@ -14,14 +14,12 @@ namespace UKHO.ExternalNotificationService.SubscriptionService.Helpers
     [ExcludeFromCodeCoverage] ////Excluded from code coverage as it has ADD interaction
     public class AuthTokenProvider : IAuthTokenProvider
     {
-        private readonly IOptions<D365CallbackConfiguration> _d365CallbackConfiguration;
-        private readonly IOptions<AzureADConfiguration> _azureADConfiguration;
+        private readonly IOptions<D365CallbackConfiguration> _d365CallbackConfiguration;        
         private readonly ILogger<AuthTokenProvider> _logger;
 
-        public AuthTokenProvider(IOptions<D365CallbackConfiguration> d365CallbackConfiguration, IOptions<AzureADConfiguration> azureADConfiguration, ILogger<AuthTokenProvider> logger)
+        public AuthTokenProvider(IOptions<D365CallbackConfiguration> d365CallbackConfiguration, ILogger<AuthTokenProvider> logger)
         {
             _d365CallbackConfiguration = d365CallbackConfiguration;
-            _azureADConfiguration = azureADConfiguration;
             _logger = logger;
         }
 
@@ -29,8 +27,10 @@ namespace UKHO.ExternalNotificationService.SubscriptionService.Helpers
         { 
             try
             {
-                AuthenticationResult token = await GenerateADAccessToken(_d365CallbackConfiguration.Value.D365Uri, _azureADConfiguration.Value);
-                return token.AccessToken;
+                string[] scopes = new[] { _d365CallbackConfiguration.Value.D365Uri + "/.default" };
+                var defaultCredential = new DefaultAzureCredential();
+                AccessToken tokenResponse = await defaultCredential.GetTokenAsync(new TokenRequestContext(scopes));
+                return tokenResponse.Token;
             }
             catch (Exception ex)
             {
@@ -38,19 +38,6 @@ namespace UKHO.ExternalNotificationService.SubscriptionService.Helpers
                "AD Authentication failed with message:{ex} for SubscriptionId:{SubscriptionId} and _D365-Correlation-ID:{correlationId} and _X-Correlation-ID:{CorrelationId}", ex.Message, subscriptionMessage.SubscriptionId, subscriptionMessage.D365CorrelationId, subscriptionMessage.CorrelationId);
                 return string.Empty;
             }             
-        }
-
-        private static async Task<AuthenticationResult> GenerateADAccessToken(string d365Uri, AzureADConfiguration ensAuthConfiguration)
-        {
-            string[] scopes = new string[] { d365Uri + "/.default" };
-
-            IConfidentialClientApplication app = ConfidentialClientApplicationBuilder.Create(ensAuthConfiguration.ClientId)
-                                                .WithClientSecret(ensAuthConfiguration.ClientSecret)
-                                                .WithAuthority(new Uri(ensAuthConfiguration.MicrosoftOnlineLoginUrl + ensAuthConfiguration.TenantId))
-                                                .Build();
-
-            AuthenticationResult tokenTask = await app.AcquireTokenForClient(scopes).ExecuteAsync();            
-            return tokenTask;
-        }
+        }        
     }
 }
