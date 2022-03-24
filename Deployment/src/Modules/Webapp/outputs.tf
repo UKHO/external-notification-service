ee56795e8bb6a0402d650d473043d7894b277ad1
Tejasi Chavan <tejasi15072@mastek.com>
2022-03-24 13:24:11 +0530

    terraform changes to delete stub deployment in QA and rename Api name in live apim

--- Deployment/src/Modules/Webapp/outputs.tf
+++ Deployment/src/Modules/Webapp/outputs.tf
@@ -21,9 +21,9 @@ output "webapp_scm_credentials"{
 }
 
 output "webapp_name" {
-  value = azurerm_app_service.stub_webapp_service.name
+  value = var.env_name == "dev" ? azurerm_app_service.stub_webapp_service[0].name : null
 }
 
 output "default_site_hostname_ens_stub" {
-  value = azurerm_app_service.stub_webapp_service.default_site_hostname
+  value = var.env_name == "dev" ? azurerm_app_service.stub_webapp_service[0].default_site_hostname : null
 }
