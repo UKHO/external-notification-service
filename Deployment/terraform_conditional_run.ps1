ffb6f7705aa7452fd2cd78935093a41c5e31dcbf
Tejasi Chavan <tejasi15072@mastek.com>
2022-04-09 01:13:15 +0530

    terraform changes for testing #AB45012

--- Deployment/terraform_conditional_run.ps1
+++ Deployment/terraform_conditional_run.ps1
@@ -61,7 +61,7 @@ Write-Host "##vso[task.setvariable variable=EnsStorageQueueName]$($terraformOutp
 Write-Host "##vso[task.setvariable variable=SubscriptionStorageConfiguration.StorageContainerName]$($terraformOutput.dead_letter_storage_container.value)"
 Write-Host "##vso[task.setvariable variable=stubWebAppName]$($terraformOutput.stub_webappname.value)"
 Write-Host "##vso[task.setvariable variable=D365ApiStubUri]$($terraformOutput.ens_stub_web_app_url.value)"
-Write-Host "##vso[task.setvariable variable=ResourceGroup]$($terraformOutput.webapp_rg.value)"
+Write-Host "##vso[task.setvariable variable=ResourceGroup;isOutput=true]$($terraformOutput.webapp_rg.value)"
 Write-Host "##vso[task.setvariable variable=EventGridDomainConfiguration.EventGridDomainEndpoint;issecret=true]$($terraformOutput.event_grid_domain_endpoint_url.value)"
 
 $terraformOutput | ConvertTo-Json -Depth 5 > $terraformJsonOutputFile
