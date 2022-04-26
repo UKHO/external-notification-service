0a7d05e51b6bda81dc249d8ade68b5d87208aef4
barrie-cooper <barrie.cooper@ukho.gov.uk>
2022-04-26 17:26:07 +0100

    Abzu 46711 Change Responsible Team for Azure Resources from Mastek to Abzu (#60)
    * Change Responsible Team for Azure Resources from Mastek to Abzu
    
    * Correct service owner

--- Deployment/src/variables.tf
+++ Deployment/src/variables.tf
@@ -16,8 +16,8 @@ locals {
   tags = {
     SERVICE          = "External Notification Service"
     ENVIRONMENT      = local.env_name
-    SERVICE_OWNER    = "UKHO"
-    RESPONSIBLE_TEAM = "Mastek"
+    SERVICE_OWNER    = "Paul Tooze"
+    RESPONSIBLE_TEAM = "Abzu"
     CALLOUT_TEAM     = "On-Call_N/A"
     COST_CENTRE      = "A.008.02"
   }
