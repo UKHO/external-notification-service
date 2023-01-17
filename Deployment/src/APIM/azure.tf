86938c93e03a960ebb92497a0814a4aa1c23e826
Jivitesh Trivedi <ajivitesh@ukho.gov.uk>
2023-01-17 11:51:59 +0000

    ABZU-65225-udpated terrform and Azure RM versions Updated NVDSuppressions

--- Deployment/src/APIM/azure.tf
+++ Deployment/src/APIM/azure.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.91.0"
+      version = "=3.0.1"
     }
   }
 
-  required_version = "=1.1.4"
+  required_version = "=1.3.7"
   backend "azurerm" {
     container_name = "tfstate"
     key            = "terraform.apim.tfplan"
