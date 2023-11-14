11eccf6791f58b6fa81a8522740a490a69560a7e
Richard Hernandez <richard.hernandez@ukho.gov.uk>
2023-11-14 12:11:58 +0000

    Reduce wait time for deadlettercallback test from 8 to 2 minutes

--- UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.API.FunctionalTests/FunctionalTests/DeadLetterCallbackToD365Test.cs
+++ UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.API.FunctionalTests/FunctionalTests/DeadLetterCallbackToD365Test.cs
@@ -48,10 +48,12 @@ namespace UKHO.ExternalNotificationService.API.FunctionalTests.FunctionalTests
             HttpResponseMessage apiResponse = await EnsApiClient.PostEnsWebhookNewEventPublishedAsync(ensWebhookJson, EnsToken);
             DateTime startTime = DateTime.UtcNow;
 
-            while (DateTime.UtcNow - startTime < TimeSpan.FromSeconds(TestConfig.WaitingTimeForQueueInSeconds))
-            {
-                await Task.Delay(10000);
-            }
+            //while (DateTime.UtcNow - startTime < TimeSpan.FromSeconds(TestConfig.WaitingTimeForQueueInSeconds))
+            //{
+            //    await Task.Delay(10000);
+            //}
+
+            await Task.Delay(TestConfig.WaitingTimeForQueueInSeconds * 1000);
 
             Assert.AreEqual(200, (int)apiResponse.StatusCode, $"Incorrect status code {apiResponse.StatusCode} is returned, instead of the expected 200.");
             HttpResponseMessage stubResponse = await StubApiClient.GetStubApiCacheReturnStatusAsync(subject, EnsToken);
@@ -65,7 +67,8 @@ namespace UKHO.ExternalNotificationService.API.FunctionalTests.FunctionalTests
             Assert.Greater(getMatchingData.Count(), 1);
 
             DateTime requestTime = DateTime.UtcNow;
-            await Task.Delay(420000);
+            //await Task.Delay(420000);
+            await Task.Delay(TestConfig.WaitingTimeForQueueInSeconds * 1000);
 
             HttpResponseMessage callBackResponse = await EnsApiClient.GetEnsCallBackAsync(TestConfig.StubBaseUri, subscriptionId.ToUpper());
 
