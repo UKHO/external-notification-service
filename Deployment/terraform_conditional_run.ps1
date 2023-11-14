0ee911d3605eed3b6fe7e95d1dc5daa9ba78c417
martyn-fewtrell <martyn.fewtrell@ukho.gov.uk>
2023-11-14 17:04:48 +0000

    Corected parameter format

--- Deployment/terraform_conditional_run.ps1
+++ Deployment/terraform_conditional_run.ps1
@@ -31,7 +31,7 @@ terraform validate
 if ( !$? ) { echo "Something went wrong during terraform validation" ; throw "Error" }
 
 Write-output "Execute Terraform plan"
-terraform plan -out "terraform.deployment.tfplan" -elasticApmServerUrl $(ElasticAPM.ServerURL) -elasticApmApiKey $(ElasticAPM.ApiKey) -elasticApmEnvironment $(ElasticAPM.Environment) | tee terraform_output.txt
+terraform plan -out "terraform.deployment.tfplan" -var elastic_apm_server_url=$elasticApmServerUrl -var elastic_apm_api_key=$elasticApmApiKey -var elastic_apm_environment=$elasticApmEnvironment | tee terraform_output.txt
 if ( !$? ) { echo "Something went wrong during terraform plan" ; throw "Error" }
 
 $totalDestroyLines=(Get-Content -Path terraform_output.txt | Select-String -Pattern "destroy" -CaseSensitive |  where {$_ -ne ""}).length
