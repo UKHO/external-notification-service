4d0f14fad3f7cf58d45cc190288725eb671e6594
HugoBurgess <hugo.burgess@ukho.gov.uk>
2024-03-05 16:09:47 +0000

    Import

--- Deployment/terraform_remove.ps1
+++ Deployment/terraform_remove.ps1
@@ -30,40 +30,55 @@ if ( !$? ) { echo "Error while selecting workspace"; throw "Error" }
 Write-Output "Checking if old style resources are present..."
 
 $webApp1=terraform state list | Select-String "module.webapp_service.azurerm_app_service.webapp_service"
+$newWebApp1='module.webapp_service.azurerm_windows_web_app.webapp_service'
 $webApp2=terraform state list | Select-String "module.webapp_service.azurerm_app_service_slot.staging"
+$newWebApp2='module.webapp_service.azurerm_windows_web_app_slot.staging'
 $webApp3=terraform state list | Select-String "module.webapp_service.azurerm_app_service.stub_webapp_service"
+$newWebApp3='module.webapp_service.azurerm_windows_web_app.stub_webapp_service'
 
 Write-Output "Output WebApp1: $webApp1"
 Write-Output "Output WebApp2: $webApp2"
 Write-Output "Output WebApp3: $webApp3"
 
-if (![string]::IsNullOrWhiteSpace($webApp1)) 
+if (![string]::IsNullOrWhiteSpace($webApp1))
 {
-    Write-Output "$webApp1 present in state file - removing it..." 
-    #terraform state rm "module.webapp_service.azurerm_app_service.webapp_service"
-    Write-Output "$webApp1 removal from state file done..." 
+    Write-Output "$webApp1 present in state file - removing it..."
+    terraform state rm "module.webapp_service.azurerm_app_service.webapp_service"
+    Write-Output "$webApp1 removal from state file done..."
+    Write-Output "$newWebApp1 importing to state file..."
+    terraform import -var elastic_apm_server_url=$elasticApmServerUrl -var elastic_apm_api_key=$elasticApmApiKey "$newWebApp1" "/subscriptions/db328b32-0563-44a5-8f51-689c5cae8a7e/resourceGroups/ens-dev-rg/providers/Microsoft.Web/serverfarms/ens-dev-webapp-asp"
+    if ( !$? ) { echo "Something went wrong during terraform import"; throw "Error" }
+    Write-Output "$newWebApp1 import done..."
 }
 else
 {
     Write-Output "$webApp1 not present in state file..."
 }
 
-if (![string]::IsNullOrWhiteSpace($webApp2)) 
+if (![string]::IsNullOrWhiteSpace($webApp2))
 {
-    Write-Output "$webApp2 present in state file - removing it..." 
-    #terraform state rm "module.webapp_service.azurerm_app_service_slot.staging"
-    Write-Output "$webApp2 removal from state file done..." 
+    Write-Output "$webApp2 present in state file - removing it..."
+    terraform state rm "module.webapp_service.azurerm_app_service_slot.staging"
+    Write-Output "$webApp2 removal from state file done..."
+    Write-Output "$newWebApp2 importing to state file..."
+    terraform import -var elastic_apm_server_url=$elasticApmServerUrl -var elastic_apm_api_key=$elasticApmApiKey "$newWebApp2" "/subscriptions/db328b32-0563-44a5-8f51-689c5cae8a7e/resourceGroups/ens-dev-rg/providers/Microsoft.Web/serverfarms/ens-dev-webapp-asp"
+    if ( !$? ) { echo "Something went wrong during terraform import"; throw "Error" }
+    Write-Output "$newWebApp2 import done..."
 }
 else
 {
     Write-Output "$webApp2 not present in state file..."
 }
 
-if (![string]::IsNullOrWhiteSpace($webApp3)) 
+if (![string]::IsNullOrWhiteSpace($webApp3))
 {
-    Write-Output "$webApp3 present in state file - removing it..." 
-    #terraform state rm "module.webapp_service.azurerm_app_service.stub_webapp_service"
-    Write-Output "$webApp3 removal from state file done..." 
+    Write-Output "$webApp3 present in state file - removing it..."
+    terraform state rm "module.webapp_service.azurerm_app_service.stub_webapp_service"
+    Write-Output "$webApp3 removal from state file done..."
+    Write-Output "$newWebApp3 importing to state file..."
+    terraform import -var elastic_apm_server_url=$elasticApmServerUrl -var elastic_apm_api_key=$elasticApmApiKey "$newWebApp3" "/subscriptions/db328b32-0563-44a5-8f51-689c5cae8a7e/resourceGroups/ens-dev-rg/providers/Microsoft.Web/serverfarms/ens-dev-webapp-asp"
+    if ( !$? ) { echo "Something went wrong during terraform import"; throw "Error" }
+    Write-Output "$newWebApp3 import done..."
 }
 else
 {
