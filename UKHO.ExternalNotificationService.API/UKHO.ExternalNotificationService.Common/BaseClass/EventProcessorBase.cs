b4ec32eab5a5d002dd0cbec7fe61a5b28a35998e
JiviteshT <112479261+JiviteshT@users.noreply.github.com>
2024-07-18 08:35:19 +0100

    Abzu-164809 - Add event publish delay configuration (#164)
    This commit introduces a new configuration for controlling the delay before publishing events, aiming to enhance the flexibility and reliability of the event processing system. The key changes include:
    
    - Updated `appsettings.json` to include `EventProcessorConfiguration` with a `ScsEventPublishDelayInSeconds` property, initially set to 2, later updated to 10 seconds.
    - Modified `ScsEventProcessor.cs` and `ScsEventProcessorTest.cs` to utilize the new configuration, adding necessary `using` directives, a private configuration field, and adjusting the constructor to accept the configuration as a parameter. Also, implemented logic to calculate the delay in milliseconds, ensuring it falls within defined min and max bounds.
    - Added `EventProcessorConfiguration.cs` class to define the configuration structure.
    - Updated `Startup.cs` to bind the new configuration to the `EventProcessorConfiguration` class, making it available through dependency injection.
    - Enhanced testing by adding new test cases in `ScsEventProcessorTest.cs` and `EventProcessorBaseTest.cs` to verify the behavior of the event processor with different configured delays and the successful delay of event publication, respectively.

--- UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.Common/BaseClass/EventProcessorBase.cs
+++ UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.Common/BaseClass/EventProcessorBase.cs
@@ -1,4 +1,5 @@
-﻿using Azure.Messaging;
+﻿using System;
+using Azure.Messaging;
 using System.Threading;
 using System.Threading.Tasks;
 using UKHO.ExternalNotificationService.Common.Helpers;
@@ -22,6 +23,13 @@ namespace UKHO.ExternalNotificationService.Common.BaseClass
         public async Task PublishEventAsync(CloudEvent cloudEvent, string correlationId, CancellationToken cancellationToken = default)
         {
             await _azureEventGridDomainService.PublishEventAsync(cloudEvent, correlationId, cancellationToken);
+        }
+        
+        public async Task PublishEventWithDelayAsync(CloudEvent cloudEvent, string correlationId, int millisecondsDelay,  CancellationToken cancellationToken = default)
+        {
+            Thread.Sleep(Math.Max(0, millisecondsDelay));   
+            
+            await PublishEventAsync(cloudEvent, correlationId, cancellationToken);
         }
     }
 }
