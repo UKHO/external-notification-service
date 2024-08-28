95c882c2eed2ed3ec52aaecc35a6fdf0db061b4c
HugoBurgess <hugo.burgess@ukho.gov.uk>
2024-08-28 15:26:01 +0100

    Move migration

---
+++ Deployment/src/migration.tf
@@ -0,0 +1,52 @@
+# Used to migrate deprecated resources without having to destroy and recreate them.
+# This file can be removed once the migration is complete in all environments, but will do no harm if left.
+# See PBI #145964.
+
+# webapp
+removed {
+  from = module.webapp_service.azurerm_app_service.webapp_service
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+import {
+  to = module.webapp_service.azurerm_windows_web_app.webapp_service
+  id = "${azurerm_resource_group.rg.id}/providers/Microsoft.Web/sites/${var.name}"
+}
+
+# staging
+removed {
+  from = module.webapp_service.azurerm_app_service_slot.staging
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+import {
+  to = module.webapp_service.azurerm_windows_web_app_slot.staging
+  id = "${azurerm_resource_group.rg.id}/providers/Microsoft.Web/sites/${var.name}/slots/staging"
+}
+
+# stub (non-live only)
+removed {
+  from = module.webapp_service.azurerm_app_service.stub_webapp_service
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+locals {
+  stubs = (
+    var.env_name == "live" ? [] : [0]
+  )
+}
+
+import {
+  for_each = local.stubs
+  to    = module.webapp_service.azurerm_windows_web_app.stub_webapp_service[each.key]
+  id    = "${azurerm_resource_group.rg.id}/providers/Microsoft.Web/sites/${var.name}-stub"
+}
