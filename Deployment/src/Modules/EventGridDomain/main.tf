453feca8e13a72796264ac22c15c891a5519bc3f
HugoBurgess <hugo.burgess@ukho.gov.uk>
2024-11-26 16:02:18 +0000

    Update egdiagnosticsetting

--- Deployment/src/Modules/EventGridDomain/main.tf
+++ Deployment/src/Modules/EventGridDomain/main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_eventgrid_domain" "eventgrid_domain" {
-  name                = var.name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  tags                = var.tags
-  input_schema        = "CloudEventSchemaV1_0"
-  public_network_access_enabled   = false
+  name                          = var.name
+  location                      = var.location
+  resource_group_name           = var.resource_group_name
+  tags                          = var.tags
+  input_schema                  = "CloudEventSchemaV1_0"
+  public_network_access_enabled = false
 }
 
 resource "azurerm_role_assignment" "eventgrid_domain_role" {
@@ -12,29 +12,18 @@ resource "azurerm_role_assignment" "eventgrid_domain_role" {
   role_definition_name = "EventGrid Contributor"
   principal_id         = var.webapp_principal_id
 }
-#deprecated Argument will be removed in v4 `log` has been superseded by `enabled_log`
+
 resource "azurerm_monitor_diagnostic_setting" "egdiagnosticsetting" {
   name                           = "${var.name}-diagnostic"
-  target_resource_id             = azurerm_eventgrid_domain.eventgrid_domain.id 
+  target_resource_id             = azurerm_eventgrid_domain.eventgrid_domain.id
   eventhub_name                  = var.eventhub_name
   eventhub_authorization_rule_id = var.eventhub_namespace_authorization_rule_id
 
-  log {
+  enabled_log {
     category = "DeliveryFailures"
-    enabled  = true
+  }
 
-    retention_policy {
-      enabled = true
-    }
-    }
-
-  log {
+  enabled_log {
     category = "PublishFailures"
-    enabled  = true
-
-    retention_policy {
-      enabled = true
-    }
   }
 }
-
