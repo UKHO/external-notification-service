f70a67127bb45d9e84d02ddd8ed9e35b63d604d2
richardAhz <Richard.Hernandez@ukho.gov.uk>
2025-05-01 12:07:48 +0100

    Set possible nul strings to empty in ElasticLogDocument class

--- UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.Common/Models/Monitoring/ElasticLogDocument.cs
+++ UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.Common/Models/Monitoring/ElasticLogDocument.cs
@@ -7,17 +7,17 @@ public class ElasticLogDocument
     [JsonPropertyName("@timestamp")]
     public DateTime Timestamp { get; set; }
     public string Id { get; set; }
-    public string ProductName { get; set; }
+    public string ProductName { get; set; } = string.Empty;
     public int? EditionNumber { get; set; }
     public int UpdateNumber { get; set; }
-    public string StatusName { get; set; }
-    public string EventType { get; set; }
-    public string TraceId { get; set; }
+    public string StatusName { get; set; } = string.Empty;
+    public string EventType { get; set; } = string.Empty;
+    public string TraceId { get; set; } = string.Empty;
     public DateTime? StartTimestamp { get; set; }
     public DateTime? StopTimestamp { get; set; }
     public int? Duration { get; set; }
     public bool? ImmediateRelease { get; set; }
-    public string Environment { get; set; }
+    public string Environment { get; set; } = string.Empty;
     public bool IsAbnormal { get; set; }
     public bool IsComplete { get; set; }
 }
