9f1ec6cc9dd9185ea0a93ca7065298e07e0c44aa
srjames-ukho <126559680+srjames-ukho@users.noreply.github.com>
2025-09-18 14:46:51 +0100

    Fix unit tests

--- UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.API.UnitTests/Services/AddsElasticMonitoringTest.cs
+++ UKHO.ExternalNotificationService.API/UKHO.ExternalNotificationService.API.UnitTests/Services/AddsElasticMonitoringTest.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Elastic.Clients.Elasticsearch;
+using Elastic.Transport;
 using FakeItEasy;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -19,6 +20,8 @@ public class AddsElasticMonitoringTest
     private IOptions<ElasticApmConfiguration> _elasticApmConfiguration;
     private ElasticsearchClient _fakeElasticSearchClient;
     private AddsElasticMonitoringService _addsElasticMonitoringService;
+    private AddsData _addsData;
+    private string _correlationId;
 
     [SetUp]
     public void SetUp()
@@ -26,106 +29,110 @@ public class AddsElasticMonitoringTest
         _fakeLogger = A.Fake<ILogger<AddsElasticMonitoringService>>();
         _elasticApmConfiguration = Options.Create(new ElasticApmConfiguration
         {
-            Environment = "TestEnvironment",
-            IndexName = "TestIndex"
+            Environment = "TestEnvironment", IndexName = "TestIndex"
         });
         _fakeElasticSearchClient = A.Fake<ElasticsearchClient>();
-        _addsElasticMonitoringService = new AddsElasticMonitoringService(_fakeLogger, _elasticApmConfiguration, _fakeElasticSearchClient);
+        _addsElasticMonitoringService =
+            new AddsElasticMonitoringService(_fakeLogger, _elasticApmConfiguration, _fakeElasticSearchClient);
+        _addsData = new AddsData
+        {
+            ProductName = "TestProduct",
+            EditionNumber = 1,
+            UpdateNumber = 1,
+            StatusName = "TestStatus",
+            Type = "TestType"
+        };
+        _correlationId = "TestCorrelationId";
     }
 
     [Test]
     public async Task StopProcessAsync_ShouldSetElasticDocumentDurationAndIsCompleteTrue()
     {
-        AddsData addsData = new()
-        {
-            ProductName = "TestProduct",
-            EditionNumber = 1,
-            UpdateNumber = 1,
-            StatusName = "TestStatus",
-            Type = "TestType"
-        };
+        // Arrange
+        ConfigureElasticClientGetAsyncWithValidResponse();
 
-        string correlationId = "TestCorrelationId";
+        // Act
+        await _addsElasticMonitoringService.StopProcessAsync(_addsData, _correlationId);
 
-        ElasticLogDocument document = new()
-        {
-            StartTimestamp = DateTime.UtcNow.AddMinutes(-5), ImmediateRelease = true, Id = "TestDocumentId", IsComplete = false, Duration = null
-        };
-
-        GetResponse<ElasticLogDocument> startProcessResponse = new() { Source = document };
-       
-        A.CallTo(() => _fakeElasticSearchClient.GetAsync(A<Id>.Ignored, A<Action<GetRequestDescriptor<ElasticLogDocument>>>.Ignored, A<CancellationToken>.Ignored))
-            .Returns(startProcessResponse);
-
+        // Assert
         Func<ElasticLogDocument, bool> documentMatcher = d =>
         {
-            Assert.That(d.IsComplete, Is.True, "Is Complete should be true");
-            Assert.That(d.Duration.HasValue,  Is.True, "Duration should not be null");
-            Assert.That(d.Duration ?? 0, Is.GreaterThan(TimeSpan.Zero.Seconds), "Duration should be greater than zero");
-            
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(d.IsComplete, Is.True, "Is Complete should be true");
+                Assert.That(d.Duration.HasValue, Is.True, "Duration should not be null");
+                Assert.That(d.Duration ?? 0, Is.GreaterThan(TimeSpan.Zero.Seconds), "Duration should be greater than zero");
+            }
             return true;
         };
-        
-        await _addsElasticMonitoringService.StopProcessAsync(addsData, correlationId);
 
-        A.CallTo(() => _fakeElasticSearchClient.IndexAsync(A<ElasticLogDocument>.That.Matches(d => documentMatcher(d)), A<Action<IndexRequestDescriptor<ElasticLogDocument>>>.Ignored, A<CancellationToken>.Ignored))
+        A.CallTo(() => _fakeElasticSearchClient.IndexAsync(A<ElasticLogDocument>.That.Matches(d => documentMatcher(d)),
+                A<Action<IndexRequestDescriptor<ElasticLogDocument>>>.Ignored, A<CancellationToken>.Ignored))
             .MustHaveHappenedOnceExactly();
 
         A.CallTo(_fakeLogger).Where(call => call.GetArgument<LogLevel>(0) == LogLevel.Information)
-            .MustHaveHappened(2, Times.Exactly);
+            .MustHaveHappened(3, Times.Exactly);
+
+        return;
+
+        void ConfigureElasticClientGetAsyncWithValidResponse()
+        {
+            ElasticLogDocument document = new()
+            {
+                StartTimestamp = DateTime.UtcNow.AddMinutes(-5),
+                ImmediateRelease = true,
+                Id = "TestDocumentId",
+                IsComplete = false,
+                Duration = null
+            };
+
+            GetResponse<ElasticLogDocument> startProcessResponse = new() { Source = document };
+
+            A.CallTo(() => _fakeElasticSearchClient.GetAsync(A<Id>.Ignored,
+                    A<Action<GetRequestDescriptor<ElasticLogDocument>>>.Ignored, A<CancellationToken>.Ignored))
+                .Returns(startProcessResponse);
+        }
     }
 
     [Test]
-    public async Task StopProcessAsync_ShouldLogWarningWhenProcessIsFailed()
+    public async Task StopProcessAsync_ShouldLogErrorWhenProcessIsFailed()
     {
-        AddsData addsData = new()
-        {
-            ProductName = "TestProduct",
-            EditionNumber = 1,
-            UpdateNumber = 1,
-            StatusName = "TestStatus",
-            Type = "TestType"
-        };
-
-        string correlationId = "TestCorrelationId";
-
-        A.CallTo(() => _fakeElasticSearchClient.GetAsync(A<Id>.Ignored, A<Action<GetRequestDescriptor<ElasticLogDocument>>>.Ignored, A<CancellationToken>.Ignored))
+        // Arrange
+        A.CallTo(() => _fakeElasticSearchClient.GetAsync(A<Id>.Ignored,
+                A<Action<GetRequestDescriptor<ElasticLogDocument>>>.Ignored, A<CancellationToken>.Ignored))
             .ThrowsAsync(new Exception("failed"));
 
-        await _addsElasticMonitoringService.StopProcessAsync(addsData, correlationId);
+        // Act
+        await _addsElasticMonitoringService.StopProcessAsync(_addsData, _correlationId);
 
-        A.CallTo(() => _fakeElasticSearchClient.IndexAsync(A<ElasticLogDocument>.Ignored, A<Action<IndexRequestDescriptor<ElasticLogDocument>>>.Ignored, A<CancellationToken>.Ignored))
+        // Assert
+        A.CallTo(() => _fakeElasticSearchClient.IndexAsync(A<ElasticLogDocument>.Ignored,
+                A<Action<IndexRequestDescriptor<ElasticLogDocument>>>.Ignored, A<CancellationToken>.Ignored))
             .MustNotHaveHappened();
-        
+
         A.CallTo(_fakeLogger).Where(call => call.GetArgument<LogLevel>(0) == LogLevel.Information)
             .MustHaveHappened(1, Times.Exactly);
 
-        A.CallTo(_fakeLogger).Where(call => call.GetArgument<LogLevel>(0) == LogLevel.Warning)
+        A.CallTo(_fakeLogger).Where(call => call.GetArgument<LogLevel>(0) == LogLevel.Error)
             .MustHaveHappened(1, Times.Exactly);
     }
 
     [Test]
     public async Task StopProcessAsync_ShouldLogWarningWhenElasticsearchClientIsNull()
     {
-        AddsData addsData = new()
-        {
-            ProductName = "TestProduct",
-            EditionNumber = 1,
-            UpdateNumber = 1,
-            StatusName = "TestStatus",
-            Type = "TestType"
-        };
-
-        string correlationId = "TestCorrelationId";
-
+        // Arrange
         _addsElasticMonitoringService = new AddsElasticMonitoringService(_fakeLogger, _elasticApmConfiguration);
 
-        await _addsElasticMonitoringService.StopProcessAsync(addsData, correlationId);
+        // Act
+        await _addsElasticMonitoringService.StopProcessAsync(_addsData, _correlationId);
 
-        A.CallTo(() => _fakeElasticSearchClient.IndexAsync(A<ElasticLogDocument>.Ignored, A<Action<IndexRequestDescriptor<ElasticLogDocument>>>.Ignored, A<CancellationToken>.Ignored))
+        // Assert
+        A.CallTo(() => _fakeElasticSearchClient.IndexAsync(A<ElasticLogDocument>.Ignored,
+                A<Action<IndexRequestDescriptor<ElasticLogDocument>>>.Ignored, A<CancellationToken>.Ignored))
             .MustNotHaveHappened();
 
-        A.CallTo(() => _fakeElasticSearchClient.GetAsync(A<Id>.Ignored, A<Action<GetRequestDescriptor<ElasticLogDocument>>>.Ignored, A<CancellationToken>.Ignored))
+        A.CallTo(() => _fakeElasticSearchClient.GetAsync(A<Id>.Ignored,
+                A<Action<GetRequestDescriptor<ElasticLogDocument>>>.Ignored, A<CancellationToken>.Ignored))
             .MustNotHaveHappened();
 
         A.CallTo(_fakeLogger).Where(call => call.GetArgument<LogLevel>(0) == LogLevel.Warning)
