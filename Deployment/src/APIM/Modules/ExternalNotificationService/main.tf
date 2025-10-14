56d5da56a53ef204abebbe0259171cdf04155f3c
HugoBurgess <hugo.burgess@ukho.gov.uk>
2025-10-14 17:09:43 +0100

    Link APIM to key vault

--- Deployment/src/APIM/Modules/ExternalNotificationService/main.tf
+++ Deployment/src/APIM/Modules/ExternalNotificationService/main.tf
@@ -94,12 +94,13 @@ resource "azurerm_api_management_product_group" "ees_product_group_mappping" {
 
 # Named value to store secret used for creating ENS token
 resource "azurerm_api_management_named_value" "ens_token_secret_named_value" {
-  name                = "ens-token-secret-${local.formatted_env}"
-  resource_group_name = data.azurerm_api_management.apim_instance.resource_group_name
-  api_management_name = data.azurerm_api_management.apim_instance.name
-  display_name        = "ens-token-secret-${local.formatted_env}"
-  value               = var.client_credentials_secret
-  secret              = true
+  name                 = "ens-token-secret-${local.formatted_env}"
+  resource_group_name  = data.azurerm_api_management.apim_instance.resource_group_name
+  api_management_name  = data.azurerm_api_management.apim_instance.name
+  display_name         = "ens-token-secret-${local.formatted_env}"
+  value_from_key_vault {
+      secret_id = var.client_credentials_secret_id
+  }
 }
 
 #D365 Product policy
