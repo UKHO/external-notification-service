da98382d9e68b4803b6d0c89d5d911f8128ae494
Jivitesh Trivedi <ajivitesh@ukho.gov.uk>
2026-07-17 12:18:35 +0100

    Add windows_fx_version to web app site_config
    Explicitly set windows_fx_version to "DOTNET|10.0" in the azurerm_windows_web_app resource to define the runtime stack for the web app. No other logic was modified.

--- Deployment/src/Modules/Webapp/main.tf
+++ Deployment/src/Modules/Webapp/main.tf
@@ -18,8 +18,10 @@ resource "azurerm_windows_web_app" "webapp_service" {
   site_config {
     application_stack {
       current_stack = "dotnet"
-      dotnet_version = "v10.0"
+      dotnet_version = "v10.0"     
     }
+
+    windows_fx_version = "DOTNET|10.0"
     
     always_on         = true
     ftps_state        = "Disabled"
